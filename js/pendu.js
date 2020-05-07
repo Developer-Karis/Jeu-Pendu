84770ec871c0b4878b229a23dac82062b611c1ef
Karis Papadopoulos <papadopouloskaris@gmail.com>
2020-05-07 17:09:31 +0200

    update 07/05 17:09

--- js/pendu.js
+++ js/pendu.js
@@ -9,19 +9,22 @@ let dictionnaire = ["terminal", "processeur", "informatique", "peripheriques", "
     "rideau", "robinet", "salle", "savon", "serrure", "serviette", "siege", "sieste", "silence", "sol", "sommeil", "sonnette", "sortie", "table",
     "tableau", "tabouret", "tapis", "tiroir", "toilette", "vitre", "wc"];
 
-let motRandom = motsAleatoires().toUpperCase();     // Mot générer aléatoirement
-let mauvaiseLettre = 0;                             // Permet de compter le nombre de mauvaise lettres 
-let lettresTrouvees = 0;                            // Permet de mettre toutes les lettres trouvées dans un tableau.
-let score = 0;                                      // Permet d'incrémenter le score lorsque le joueur gagne la partie
-let fini = false;                                   // Indique si le jeu est fini ou pas.
-let nomJoueur;                                      // Variable du prompt - Demande le nom du Joueur
-let essais = 8;                                     // Nombre d'essais pour le joueur
+let motRandom = motsAleatoires().toUpperCase();         // Mot générer aléatoirement
+let mauvaiseLettre = 0;                                 // Permet de compter le nombre de mauvaise lettres 
+let lettresTrouvees = 0;                                // Permet de mettre toutes les lettres trouvées dans un tableau.
+let score = 0;                                          // Permet d'incrémenter le score lorsque le joueur gagne la partie
+let fini = false;                                       // Indique si le jeu est fini ou pas.
+let nomJoueur;                                          // Variable du prompt - Demande le nom du Joueur
+let essais = 8;                                         // Nombre d'essais pour le joueur
+let secondes = 60;                                      // Temps restant du joueur
+let interval = setInterval(timer, 1000);                // Ajoute un timer
 
 /**
  * Permet de lancer le jeu
  */
 function startGame() {
-    gameAudio();
+    let accueil = document.getElementById('accueil');
+    accueil.volume = 0.1;
     let tirets = "?";
     let newSpan = document.getElementById("mettreMot");
     for (let i in motRandom) {
@@ -29,6 +32,7 @@ function startGame() {
     }
     document.getElementById('tentatives').innerHTML = "Nombre d'essais : " + essais;
     document.getElementById('infos').innerHTML = 'Cliquer sur une lettre juste en dessous pour commencer à jouer !';
+    document.getElementById('timer').innerHTML = "Temps restant : " + secondes + " secondes";
 }
 
 /**
@@ -58,10 +62,18 @@ function choisirLettre(lettre) {
             lettre.disabled = true;
             lettresTrouvees++;
             score++;
+            let bonneReponse = document.getElementById('bonneReponse');
+            bonneReponse.volume = 0.1;
+            accueil.pause();
+            bonneReponse.play();
         }
     }
 
     if (!motTrouvee) {
+        let mauvaiseReponse = document.getElementById('mauvaiseReponse');
+        mauvaiseReponse.volume = 0.1;
+        accueil.pause();
+        mauvaiseReponse.play();
         essais--;
         document.getElementById('tentatives').innerHTML = "Nombre d'essais : " + essais;
         mauvaiseLettre++;
@@ -77,14 +89,23 @@ function choisirLettre(lettre) {
                 document.getElementById(i).innerHTML = motRandom.charAt(i);
             }
             fini = true;
+            // Activé l'audio
             let son_perdu = document.getElementById('sonPerdu');
             son_perdu.play();
+            son_perdu.volume = 0.4;
+            // Mettre en pause la musique sur la page d'acceuil
+            accueil.pause();
+            clearInterval(interval);
         }
     }
     if (lettresTrouvees == motRandom.length) {
         document.getElementById('imagePendu').src = "images/win.gif";
         fini = true;
         score += 5;
+        let gagnant = document.getElementById('sonGagnant');
+        gagnant.volume = 0.2;
+        accueil.pause();
+        gagnant.play();
     }
 }
 
@@ -110,16 +131,12 @@ function motsAleatoires() {
 }
 
 /**
- * Permet de gérer l'audio du Jeu
- */
-function gameAudio() {
-    let accueil = document.getElementById('accueil');
-    accueil.volume = 0.2;
-}
-
-/**
  * Ajouter un timer pour rajouter de la difficulté dans le Jeu
  */
 function timer() {
-
+    if (secondes <= 30) {
+        score += 2;
+    }
+    document.getElementById('timer').innerHTML = "Temps restant : " + secondes + " secondes";
+    secondes--;
 }
